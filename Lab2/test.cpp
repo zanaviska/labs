2bff8778c8db1b55748df5210c49cdf440fb67b0
zanaviska <31745527+zanaviska@users.noreply.github.com>
2018-03-02 13:06:26 +0200

    Update test.cpp

--- Lab2/test.cpp
+++ Lab2/test.cpp
@@ -18,37 +18,55 @@ TEST_CASE("pushing element")
     for(int i = 0; i < 14; i++)
         tr.push(i, i);
     vector<int> temp;
-    get_all_values(temp);
+    tr.get_all_values(temp);
     for(int i = 0; i < 15; i++)
-        CHECK(i-1 != temp[i]);
+        CHECK(i-1 == temp[i]);
 }
 
 TEST_CASE("Nothing wrong")
 {
     Tree some_random_wrieble_that_must_has_some_strong_name;
-    CHECK(REQUIRE_NOTHROW(CHECK_THROWS(some_random_wrieble_that_must_has_some_strong_name)));
+    //CHECK(((some_random_wrieble_that_must_has_some_strong_name)));
 }
 
 TEST_CASE("getting all values")
 {
     Tree tr;
-    tr.push(8, 1);
-    tr.push(4, 2);
-    tr.push(2, 3);
-    tr.push(1, 4);
-    tr.push(3, 5);
+    tr.push(8, 8);
+    tr.push(4, 4);
+    tr.push(2, 2);
+    tr.push(1, 1);
+    tr.push(3, 3);
     tr.push(6, 6);
-    tr.push(5, 7);
-    tr.push(7, 8);
-    tr.push(12, 9);
+    tr.push(5, 5);
+    tr.push(7, 7);
+    tr.push(12, 12);
     tr.push(10, 10);
-    tr.push(9, 11);
+    tr.push(9, 9);
     tr.push(11, 11);
-    tr.push(14, 12);
+    tr.push(14, 14);
     tr.push(13, 13);
-    tr.push(15, 14);
+    tr.push(15, 15);
     vector<int> temp;
-    get_all_values(temp);
-    for(auto i:temp)
-        cout << i << ' ';
+    REQUIRE_NOTHROW(tr.get_all_values(temp));
+    for(int i = 0; i < 15; i++)
+        CHECK(i+1 == temp[i]);
+}
+
+TEST_CASE("find min")
+{
+    Tree tr;
+    for(int i = 0; i < 15; i++)
+        tr.push(i, i+1);
+    REQUIRE_NOTHROW(tr.find_min());
+    CHECK(tr.find_min() == 1);
+}
+
+TEST_CASE("find max")
+{
+    Tree tr;
+    for(int i = 0; i < 15; i++)
+        tr.push(i, i+1);
+    REQUIRE_NOTHROW(tr.find_max());
+    CHECK(tr.find_max() == 15);
 }
