b2bd0b03fd6c1995dec64489cf42a3f4d548dfac
zanaviska <31745527+zanaviska@users.noreply.github.com>
2018-03-02 10:55:44 +0200

    Update double_linked.h

--- Lab1/double_linked.h
+++ Lab1/double_linked.h
@@ -1,3 +1,5 @@
+#include<bits/stdc++.h>
+
 using namespace std;
 
 class List
@@ -10,124 +12,29 @@ class List
         ~Node(){}
     };
     int ln;// size of list
-public:
     shared_ptr<Node> first;
     shared_ptr<Node> last;
+public:
     List():first(nullptr), last(nullptr), ln(0){};
-    push_back(int new_element_data)
-    {
-        ln++;
-        auto temp = make_shared<Node>(); // new element of list
-        temp->data = new_element_data;
-        temp->next = nullptr;
-        temp->prev = nullptr;
-        if(first == nullptr)
-        {
-            first = temp;
-            last = temp;
-            return 0;
-        }
-        temp->prev = last;
-        last->next = temp;
-        last = temp;
-    }
-
-    push_front(int new_element_data)
-    {
-        ln++;
-        auto temp = make_shared<Node>(); // new element of list
-        temp->data = new_element_data;
-        temp->next = nullptr;
-        temp->prev = nullptr;
-        if(first == nullptr)
-        {
-            first = temp;
-            last = temp;
-            return 0;
-        }
-        temp->next = first;
-        first->prev = temp;
-        first = temp;
-    }
-
-    insert(int pos, int new_element_data)
-    {
-        ln++;
-        if(pos == 0) return push_front(new_element_data);
-        if(pos == ln-1) return push_back(new_element_data);
-        auto temp = make_shared<Node>(); // new element of list
-        temp->data = new_element_data;
-        shared_ptr<Node> now = first;
-        for(int i = 0; i < pos; i++)
-            now = now->next;
-        now->prev->next = temp;
-        temp->prev = now->prev;
-        now->prev = temp;
-        temp->next = now;
-    }
-
-    int size()
-    {
+    int push_back(int);
+    int push_front(int);
+    int insert(int, int);
+    int size(){
         return ln;
     }
-
-    shared_ptr<Node> begin()
-    {
+    shared_ptr<Node> begin(){
         return first;
     }
-
-    shared_ptr<Node> end()
-    {
+    shared_ptr<Node> end(){
         return last;
     }
-
-     void erase(int position)
-    {
-        if(position == 0)
-        {
-            first = first->next;
-            first->prev = nullptr;
-            return;
-        }
-        if(position == ln-1)
-        {
-            last = last->prev;
-            last->next = nullptr;
-            return;
-        }
-        shared_ptr<Node> now = first;
-        for(int i = 0; i < position; i++)
-            now = now->next;
-        now->prev->next = now->next;
-        now->next->prev = now->prev;
-    }
+    void erase(int);
 
     friend List merge(List a, List b);
 
     ~List() {}
 };
 
-void print(List a)// a is name of output List
-{
-    for(auto it = a.first; it != nullptr; it = it->next)
-    {
-        cout << it->data << "(";
-        if(it->prev != nullptr)
-            cout << it->prev->data << ", ";
-        if(it->next != nullptr)
-            cout << it->next->data;
-        cout << ")\n";
-    }
-}
+void print(List);// a is name of output List
 
-List merge(List a, List b)// merge two List in one
-{
-    if(a.first == nullptr) return b;
-    if(b.first == nullptr) return a;
-    List res = a;
-    res.last->next = b.first;
-    res.last->next->prev = res.last;
-    res.last = b.last;
-    res.ln += b.ln;
-    return res;
-}
+List merge(List, List);// merge two List in one
