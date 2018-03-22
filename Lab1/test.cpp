1910847a0e240c2be1c2420639a8a77eb0260ea9
mdapathy <md.apathy@gmail.com>
2018-03-22 22:49:34 +0200

    qqq

--- Lab1/test.cpp
+++ Lab1/test.cpp
@@ -1,104 +1,163 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "doctest.h"
 #include <bits/stdc++.h>
-#include "tree.h"
+#include "double_linked.h"
 
 using namespace std;
 
-TEST_CASE("Creating an empty tree")
+
+int f, s, t, amount, times, del;
+
+TEST_CASE("Creating an empty list")
 {
-    Tree tr;
-	CHECK(tr.size() == 0);
+
+    List list1;
+    List list2;
+    CHECK(list1.size() == 0);
+    CHECK(list2.size() == 0);
+}
+
+TEST_CASE("Accessing empty object's values")
+{
+    List list1;
+    List list2;
+    CHECK(list1.end() == nullptr);
+    CHECK(list2.end() == nullptr);
+    CHECK(list1.begin() == nullptr);
+    CHECK(list2.begin() == nullptr);
 }
 
 TEST_CASE("pushing element")
 {
-    Tree tr;
-    REQUIRE_NOTHROW(tr.push(-1, -1));
-    for(int i = 0; i < 14; i++)
-        tr.push(i, i);
-    vector<int> temp;
-    tr.get_all_values(temp);
-    for(int i = 0; i < 15; i++)
-        CHECK(i-1 == temp[i]);
+    List a;//created List with which we will work in function
+    REQUIRE_NOTHROW(a.push_back(-1));
+    for(int i = 0; i < 5; i++)
+        a.push_back(i);
+    auto it = a.begin();
+    for(int i = -1; i < 5; i++, it = it->next)
+        CHECK(it->data == i);
+
+    List b;//created List with which we will work in function
+    REQUIRE_NOTHROW(b.push_front(-1));
+    for(int i = 0; i < 5; i++)
+        b.push_front(i);
+    it = b.begin();
+    for(int i = -1; i < 5; i++, it = it->next)
+        CHECK(it->data == 3-i);
 }
 
-TEST_CASE("Nothing wrong")
+TEST_CASE("inserting element")
 {
-    Tree some_random_wrieble_that_must_has_some_strong_name;
-    //CHECK(((some_random_wrieble_that_must_has_some_strong_name)));
+    List a;//created List with which we will work in function
+    for(int i = 0; i < 10; i++)
+        a.push_back(i);
+    REQUIRE_NOTHROW(a.insert(3, 20));
+    auto now = a.begin();
+    for(int i = 0; i < 3; i++, now = now->next)
+        CHECK(now->data == i);
+    CHECK(now->data == 20);
+    now = now->next;
+    for(int i = 4; i < 11; i++, now = now->next)
+        CHECK(now->data == i-1);
 }
 
-TEST_CASE("getting all values")
+TEST_CASE("deleting element")
 {
-    Tree tr;
-    tr.push(8, 8);
-    tr.push(4, 4);
-    tr.push(2, 2);
-    tr.push(1, 1);
-    tr.push(3, 3);
-    tr.push(6, 6);
-    tr.push(5, 5);
-    tr.push(7, 7);
-    tr.push(12, 12);
-    tr.push(10, 10);
-    tr.push(9, 9);
-    tr.push(11, 11);
-    tr.push(14, 14);
-    tr.push(13, 13);
-    tr.push(15, 15);
-    vector<int> temp;
-    REQUIRE_NOTHROW(tr.get_all_values(temp));
-    for(int i = 0; i < 15; i++)
-        CHECK(i+1 == temp[i]);
+    List a;//created List with which we will work in function
+    for(int i = 0; i < 10; i++)
+        a.push_back(i);
+    CHECK_NOTHROW(a.erase(5));
+
+    auto now = a.begin();
+    for(int i = 0; i < 5; i++, now = now->next)
+        CHECK(now->data == i);
+    for(int i = 6; i < 10; i++, now = now->next)
+        CHECK(now->data == i);
 }
 
-TEST_CASE("find min")
+TEST_CASE("inserting element to negative position")
 {
-    Tree tr;
-    for(int i = 0; i < 15; i++)
-        tr.push(i, i+1);
-    REQUIRE_NOTHROW(tr.find_min());
-    CHECK(tr.find_min() == 1);
+    List list1;
+    for(int i = 0; i < 5; i++)
+        list1.push_back(i);
+    REQUIRE_THROWS(list1.insert(-5, -5));
 }
 
-TEST_CASE("find max")
+TEST_CASE("erasing element to negative position")
 {
-    Tree tr;
-    for(int i = 0; i < 15; i++)
-        tr.push(i, i+1);
-    REQUIRE_NOTHROW(tr.find_max());
-    CHECK(tr.find_max() == 15);
+    List list1;
+    for(int i = 0; i < 5; i++)
+        list1.push_back(i);
+    REQUIRE_THROWS(list1.erase(-5));
 }
 
-TEST_CASE("find all leaves")
+TEST_CASE("Merging empty and non-empty Lists")
 {
-    Tree tr;
-    tr.push(8, 9);
-    tr.push(4, 10);
-    tr.push(2, 11);
-    tr.push(1, 1);
-    tr.push(3, 2);
-    tr.push(6, 12);
-    tr.push(5, 3);
-    tr.push(7, 4);
-    tr.push(12, 13);
-    tr.push(10, 14);
-    tr.push(9, 5);
-    tr.push(11, 6);
-    tr.push(14, 15);
-    tr.push(13, 7);
-    tr.push(15, 8);
-    vector<int> temp;
-    REQUIRE_NOTHROW(tr.get_all_leave(temp));
-    for(int i = 0; i < 8; i++)
-        CHECK(temp[i] == i+1);
+    List list1 = List();//created List with which we will work in function
+    List list2 = List();
+    for(int i = 0; i < 10; i++)
+        list1.push_back(i);
+    List temp = list1;
+    temp.merge(list2);
+    CHECK(list1.begin() == temp.begin());
+    CHECK(list1.end() == temp.end());
+    CHECK(list1.size() == temp.size());
+
+    list2.merge(list1);
+    CHECK(list2.begin()->data == list1.begin()->data);
+    CHECK(list2.rbegin()->data == list1.rbegin()->data);
+    CHECK(list2.size() == list1.size());
+    int i = 0;
+    for(auto x = list1.begin(), y = list2.begin(); i < list1.size(); i++, x = x->next, y = y->next)
+        CHECK(x->data == y->data);
 }
 
-TEST_CASE("all leaves from empty tree")
+TEST_CASE("Merging empty Lists")
 {
-    Tree tr;
-    vector<int> temp;
-    REQUIRE_NOTHROW(tr.get_all_leave(temp));
-    CHECK(temp.size() == 0);
+    List a = List();//created List with which we will work in function
+    List b = List();//created 2 any List
+    a.merge(b);
+    CHECK(a.size() == 0);
+    CHECK(a.begin() == nullptr);
+}
+
+TEST_CASE("Inserting an object: empty and nonempty lists") // Checking whether the properties are up-to-date after the insertion
+{
+    int f = 10;
+    int s = 20;
+    List list1 = List();
+    List list2 = List();
+    list1.push_back(f);
+    list2.push_back(f);
+    
+    CHECK(list1.begin()->data == f);
+    CHECK(list2.begin()->data == f);
+    CHECK(list1.begin() == list1.rbegin());
+    CHECK(list2.begin() == list2.rbegin());
+    
+    list1.push_back(s);
+    list2.push_back(s);
+    
+    CHECK(list1.rbegin()->data == s);
+    CHECK(list2.rbegin()->data == s);
+}
+
+TEST_CASE("Updating list's size")
+{
+    List list1 = List();
+    List list2 = List();
+    int amount = 10;
+    for( int i = 0; i <= amount; i++)
+    {
+        list1.push_back(i);
+        list2.push_back(i);
+    }
+    int n = list1.size() + list2.size();
+
+    list2.merge(list1);
+
+    CHECK(list2.size() == n);
+    auto now = list2.begin();
+    for(int i = 0; i < n/2; i++, now = now->next)
+        CHECK(now->data == i);
 }
