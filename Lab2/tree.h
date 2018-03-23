e728436b182a5f34e1a684a504cc64d04331c570
mdapathy <md.apathy@gmail.com>
2018-03-23 11:46:58 +0200

    qq

--- Lab2/tree.h
+++ Lab2/tree.h
@@ -11,20 +11,20 @@ class Tree
         bool is_used;
     };
     Vertex* arr;
-    int ln;//size of tree;
+    int length;//size of tree;
 public:
     Tree():
-        ln(0),
+        length(0),
         arr(new Vertex[1<<20])
     {};
 
-    void push(int, int);
+    void push(int key, int value);
     int find_min(int now = 1);
     int find_max(int now = 1);
     void get_all_values(vector<int>&vec, int now = 1);
-    void get_all_leave(vector<int>&vec, int now = 1);
+    void get_all_leaf(vector<int>&vec, int now = 1);
     int size(){
-        return ln;
+        return length;
     }
     ~Tree(){
         delete []arr;
