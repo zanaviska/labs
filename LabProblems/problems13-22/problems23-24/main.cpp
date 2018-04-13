fd7b38d2dc925d848917d299729edce4e7107132
zanaviska <31745527+zanaviska@users.noreply.github.com>
2018-04-13 11:28:05 +0300

    Update main.cpp

--- LabProblems/problems13-22/problems23-24/main.cpp
+++ LabProblems/problems13-22/problems23-24/main.cpp
@@ -71,4 +71,86 @@ TEST_CASE("erasing element with the same hash")
     CHECK(table.get("bca") == 7);
 }
 
-TEST_CASE
+TEST_CASE("create empty ConsistentHash")
+{
+    REQUIRE_NOTHROW(ConsistentHash());
+    ConsistentHash gesh = ConsistentHash();
+}
+
+TEST_CASE("getting exist element and non-exist")
+{
+    ConsistentHash gesh = ConsistentHash();
+    gesh.add_node("abc", 3);
+    REQUIRE_NOTHROW(gesh.get_node("abc"));
+    CHECK(gesh.get_node("abc") == 3);
+    REQUIRE_THROWS(gesh.get_node("abd"));
+}
+
+TEST_CASE("pushing element to empty and non-empty cache")
+{
+    ConsistentHash gesh = ConsistentHash();
+    REQUIRE_THROWS(gesh.get_node("abc"));
+    gesh.add_node("abc", 6);
+    CHECK(gesh.get_node("abc") == 6);
+    gesh.add_node("abd", 7);
+    REQUIRE_NOTHROW(gesh.get_node("abc"));
+    REQUIRE_NOTHROW(gesh.get_node("abd"));
+    CHECK(gesh.get_node("abc") == 6);
+    CHECK(gesh.get_node("abd") == 7);
+}
+
+TEST_CASE("deleting node")
+{
+    ConsistentHash gesh = ConsistentHash();
+    REQUIRE_THROWS(gesh.del_node("abc"));
+    gesh.add_node("abc", 6);
+    REQUIRE_NOTHROW(gesh.get_node("abc") == 6);
+    REQUIRE_NOTHROW(gesh.del_node("abc"));
+    REQUIRE_THROWS(gesh.get_node("abc"));
+}
+
+TEST_CASE("adding server")
+{
+    ConsistentHash gesh = ConsistentHash();
+    gesh.add_node("abc", 1);//6722
+    gesh.add_node("abd", 2);//8931
+    gesh.add_node("abe", 3);//11140
+    gesh.add_server();
+    gesh.add_node("abf", 4);//13349
+    gesh.add_node("abg", 5);//15558
+    gesh.add_node("abh", 6);//17767
+    //gesh.add_server();
+    CHECK(gesh.get_node("abc") == 1);
+    CHECK(gesh.get_node("abd") == 2);
+    CHECK(gesh.get_node("abe") == 3);
+    CHECK(gesh.get_node("abf") == 4);
+    CHECK(gesh.get_node("abg") == 5);
+    CHECK(gesh.get_node("abh") == 6);
+}
+
+TEST_CASE("deleting server from empty cache")
+{
+    ConsistentHash gesh = ConsistentHash();
+    REQUIRE_THROWS(gesh.del_server());
+}
+
+
+TEST_CASE("deleting server")
+{
+    ConsistentHash gesh = ConsistentHash();
+    gesh.add_node("abc", 1);//6722
+    gesh.add_node("abd", 2);//8931
+    gesh.add_node("abe", 3);//11140
+    gesh.add_server();
+    gesh.add_node("abf", 4);//13349
+    gesh.add_node("abg", 5);//15558
+    gesh.add_node("abh", 6);//17767
+    gesh.del_server();
+    CHECK(gesh.get_node("abc") == 1);
+    CHECK(gesh.get_node("abd") == 2);
+    CHECK(gesh.get_node("abe") == 3);
+    CHECK(gesh.get_node("abf") == 4);
+    CHECK(gesh.get_node("abg") == 5);
+    CHECK(gesh.get_node("abh") == 6);
+}
+
